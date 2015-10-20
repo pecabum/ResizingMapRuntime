e451a62833d404f01f183138c585286c170e81f4
pecabum <pecabum92@gmail.com>
2015-10-20 11:05:18 +0300

    added velocity tacker

--- app/src/main/java/com/car/testingmaps2/MapsActivity.java
+++ app/src/main/java/com/car/testingmaps2/MapsActivity.java
@@ -2,11 +2,14 @@ package com.car.testingmaps2;
 
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.support.v4.view.VelocityTrackerCompat;
 import android.util.Log;
 import android.view.MotionEvent;
+import android.view.VelocityTracker;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.ViewTreeObserver;
+import android.view.animation.AnimationSet;
 import android.widget.RelativeLayout;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -20,6 +23,8 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
     private GoogleMap mMap;
     private SupportMapFragment mapFragment;
+    private VelocityTracker mVelocityTracker = null;
+
     ViewGroup rootLayout;
     private int height;
     private float dY;
@@ -73,24 +78,57 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
     @Override
     public boolean onTouch(View view, MotionEvent event) {
+        int index = event.getActionIndex();
+        int action = event.getActionMasked();
+        int pointerId = event.getPointerId(index);
 
-        switch (event.getActionMasked()) {
+        switch (action) {
 
             case MotionEvent.ACTION_DOWN:
+
                 dY = view.getY() - event.getRawY();
+                if(mVelocityTracker == null) {
+                    // Retrieve a new VelocityTracker object to watch the velocity of a motion.
+                    mVelocityTracker = VelocityTracker.obtain();
+                }
+                else {
+                    // Reset the velocity tracker back to its initial state.
+                    mVelocityTracker.clear();
+                }
+                // Add a user's movement to the tracker.
+                mVelocityTracker.addMovement(event);
                 break;
             case MotionEvent.ACTION_MOVE:
-
+                mVelocityTracker.addMovement(event);
+                // When you want to determine the velocity, call
+                // computeCurrentVelocity(). Then call getXVelocity()
+                // and getYVelocity() to retrieve the velocity for each pointer ID.
+                mVelocityTracker.computeCurrentVelocity(1000);
+                // Log velocity of pixels per second
+                // Best practice to use VelocityTrackerCompat where possible.
+                Log.d("", "X velocity: " +
+                        VelocityTrackerCompat.getXVelocity(mVelocityTracker,
+                                pointerId));
+                Log.d("", "Y velocity: " +
+                        VelocityTrackerCompat.getYVelocity(mVelocityTracker,
+                                pointerId));
                 view.animate()
                         .y(event.getRawY() + dY)
                         .setStartDelay(0).setDuration(0)
                         .start();
 
-                int lHeight = (int) (height - event.getRawY() + 30);
+                int lHeight = (int) (height - (event.getRawY() + dY));
                 RelativeLayout.LayoutParams lp = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.MATCH_PARENT, lHeight);
                 lp.addRule(RelativeLayout.ALIGN_PARENT_BOTTOM);
+                lp.topMargin = -20;
                 mapFragment.getView().setLayoutParams(lp);
                 mapFragment.getView().requestLayout();
+                break;
+
+            case MotionEvent.ACTION_UP:
+            case MotionEvent.ACTION_CANCEL:
+                mVelocityTracker.recycle();
+                mVelocityTracker = null;
                 break;
             default:
                 return false;
@@ -98,6 +136,4 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         }
         return true;
     }
-
-
 }
